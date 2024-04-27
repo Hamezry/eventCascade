1d5511f9729f7336557dbbc8b8c35cf89a6d7723
Hemense Lan <hlan@afexnigeria.com>
2024-04-27 11:26:02 +0100

    enable clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
+import "./globals.css";
 
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
-import './globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-poppins',
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
 });
 
-
 export const metadata: Metadata = {
-  title: 'EventCascade',
-  description: 'EventCascade is a platform for event management.',
+  title: "EventCascade",
+  description: "EventCascade is a platform for event management.",
   icons: {
-    icon: ''
-  }
-}
+    icon: "",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-  
+    <ClerkProvider>
       <html lang="en">
         <body className={poppins.variable}>{children}</body>
       </html>
- 
-  )
+    </ClerkProvider>
+  );
 }
